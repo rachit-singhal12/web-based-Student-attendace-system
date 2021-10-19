d9ea3a37632f15303cf3d67dce9d73a698355e5d
Rachit Singhal <88622017+rachit-singhal12@users.noreply.github.com>
2021-10-19 13:12:28 +0530

    Update scrolling-nav.js

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -1,7 +1,6 @@
 (function($) {
-  "use strict"; // Start of use strict
-
-  // Smooth scrolling using jQuery easing
+  "use strict"; 
+  
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
@@ -15,15 +14,14 @@
     }
   });
 
-  // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function() {
     $('.navbar-collapse').collapse('hide');
   });
 
-  // Activate scrollspy to add active class to navbar items on scroll
+
   $('body').scrollspy({
     target: '#mainNav',
     offset: 56
   });
 
-})(jQuery); // End of use strict
+})(jQuery); 
